51b2c8e3098e688815b710b4dd14978b5509e9a0
kulikp1 <pashakylik2@gmail.com>
2025-01-11 21:19:12 +0200

    add Welcome section page markup

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,5 +1,13 @@
 const WelcomeSection = () => {
-  return <div>WELCOME PAGE</div>;
+  return (
+    <div>
+      <h1>BudgetTrack</h1>
+      <p> Record your expenses daily and view statistics</p>
+      <h1>Record your budget</h1>
+      <button>Try</button>
+      <button>Sign In</button>
+    </div>
+  );
 };
 
 export default WelcomeSection;
