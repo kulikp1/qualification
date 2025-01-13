721c83c2b93f6d60417754c81abe2862f181357e
kulikp1 <pashakylik2@gmail.com>
2025-01-13 19:16:55 +0200

    change structure, add avatars to advantages sections

--- src/components/HomePage/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/HomePage/AdvantagesSection/AdvantagesSection.jsx
@@ -1,10 +1,20 @@
 import css from "./AdvantagesSection.module.css";
-// import backgroundImage from "../../../assets/background.jpg";
-
+import customer1 from "../../../assets/homePageAssets/pre-avatar.png";
 const AdvantagesSection = () => {
   return (
     <div className={css.mainContainer}>
-      {/* <img className={css.background} src={backgroundImage} alt="Background" /> */}
+      <div className={css.avatarContainer}>
+        <div className={css.avatars}>
+          <img src={customer1} alt="Customer 1" className={css.avatar} />
+          <img src={customer1} alt="Customer 2" className={css.avatar} />
+          <img src={customer1} alt="Customer 3" className={css.avatar} />
+        </div>
+        <div className={css.text}>
+          {/* <h2>
+            Our <span className={css.highlight}>happy</span> customers
+          </h2> */}
+        </div>
+      </div>
     </div>
   );
 };
