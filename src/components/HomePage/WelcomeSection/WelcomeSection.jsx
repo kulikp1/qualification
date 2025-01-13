fc59cb2113e9e3f44b0148f97ca25ea1dd9e4806
kulikp1 <pashakylik2@gmail.com>
2025-01-13 18:34:17 +0200

    add welcomeSection style

--- src/components/HomePage/WelcomeSection/WelcomeSection.jsx
+++ src/components/HomePage/WelcomeSection/WelcomeSection.jsx
@@ -1,11 +1,21 @@
+import css from "./WelcomeSection.module.css";
+
 const WelcomeSection = () => {
   return (
-    <div>
-      <h1>BudgetTrack</h1>
-      <p> Record your expenses daily and view statistics</p>
-      <h1>Record your budget</h1>
-      <button>Try</button>
-      <button>Sign In</button>
+    <div className={css.mainContainer}>
+      <div>
+        <h1 className={css.logo}>BudgetTrack</h1>
+      </div>
+      <div>
+        <p className={css.descr}>
+          Record your expenses daily and view statistics
+        </p>
+        <h1 className={css.title}>Record your budget</h1>
+        <div className={css.btns}>
+          <button className={css.btnSignUp}>Try tracker</button>
+          <button className={css.btnSignIn}>Sign In</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import AdvantagesSection from "../../components/HomePage/AdvantagesSection/AdvantagesSection.jsx";
+// import AdvantagesSection from "../../components/HomePage/AdvantagesSection/AdvantagesSection.jsx";
 import WelcomeSection from "../../components/HomePage/WelcomeSection/WelcomeSection.jsx";
 import css from "./HomePage.module.css";
 
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <div className={css.homePage}>
       <WelcomeSection />
-      <AdvantagesSection />
+      {/* <AdvantagesSection /> */}
     </div>
   );
 }
