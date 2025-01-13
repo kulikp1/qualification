e98b60deb90d79945620be0b64dbec4731a3c4d2
kulikp1 <pashakylik2@gmail.com>
2025-01-13 18:54:22 +0200

    add pre-background to advantages section by css & render advantages section to HomePage

---
+++ src/components/HomePage/AdvantagesSection/AdvantagesSection.jsx
@@ -0,0 +1,12 @@
+import css from "./AdvantagesSection.module.css";
+// import backgroundImage from "../../../assets/background.jpg";
+
+const AdvantagesSection = () => {
+  return (
+    <div className={css.mainContainer}>
+      {/* <img className={css.background} src={backgroundImage} alt="Background" /> */}
+    </div>
+  );
+};
+
+export default AdvantagesSection;
