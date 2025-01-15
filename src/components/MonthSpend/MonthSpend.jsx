92ea733ce58fe75ce9dbf3bd3ba6b0db35bb573b
kulikp1 <pashakylik2@gmail.com>
2025-01-15 20:51:22 +0200

    add markup for user-info window in month spend page

--- src/components/MonthSpend/MonthSpend.jsx
+++ src/components/MonthSpend/MonthSpend.jsx
@@ -1,10 +1,11 @@
 import css from "./MonthSpend.module.css";
+import UserInfo from "../UserInfo/UserInfo";
 
 const MonthsSpend = () => {
   return (
     <div className={css.Container}>
       <div>
-        <h1>Test</h1>
+        <UserInfo></UserInfo>
       </div>
     </div>
   );
---
+++ src/components/UserInfo/UserInfo.jsx
@@ -0,0 +1,17 @@
+import css from "./UserInfo.module.css";
+import avatar from "../../assets/homePageAssets/pre-avatar.png";
+import { SlArrowDown } from "react-icons/sl";
+
+const UserInfo = () => {
+  return (
+    <div className={css.Container}>
+      <div className={css.userItems}>
+        <h3>Pavlo</h3>
+        <img src={avatar} alt="avatar" className={css.avatar} />
+        <SlArrowDown className={css.icon} />
+      </div>
+    </div>
+  );
+};
+
+export default UserInfo;
