f4772743e9bde967ca59c4427ad5727e2c0edcec
kulikp1 <pashakylik2@gmail.com>
2025-01-15 21:14:52 +0200

    add markup to user info page

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -4,11 +4,17 @@ import { SlArrowDown } from "react-icons/sl";
 
 const UserInfo = () => {
   return (
-    <div className={css.Container}>
-      <div className={css.userItems}>
-        <h3>Pavlo</h3>
-        <img src={avatar} alt="avatar" className={css.avatar} />
-        <SlArrowDown className={css.icon} />
+    <div className={css.pageContainer}>
+      <div className={css.Container}>
+        <div className={css.description}>
+          <h3>Months spend</h3>
+        </div>
+
+        <div className={css.userItems}>
+          <h3>Pavlo</h3>
+          <img src={avatar} alt="avatar" className={css.avatar} />
+          <SlArrowDown className={css.icon} />
+        </div>
       </div>
     </div>
   );
