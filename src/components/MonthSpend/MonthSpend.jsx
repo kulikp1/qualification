0a27a253c827928f5fa24fb8ff6f95e0cc7c1f2d
kulikp1 <pashakylik2@gmail.com>
2025-01-15 20:26:15 +0200

    add style to MonthSpendPage

--- src/components/MonthSpend/MonthSpend.jsx
+++ src/components/MonthSpend/MonthSpend.jsx
@@ -1,20 +1,13 @@
-// import css from "./MonthSpend.css";
+import css from "./MonthSpend.module.css";
 
-// const MonthsSpend = () => {
-//   return (
-//     <div className={css.mainContainer}>
-//       <div>
-//         <h1 className={css.logo}>BudgetTrack</h1>
-//       </div>
-//       <div className={css.mainSpendsContainer}>
-//         <h1 className={css.title}>Hello, Pavlo!</h1>
-//         <h2 className={css.descr}>
-//           Today you spent 100 on the following categories:
-//         </h2>
-//       </div>
-//       <div className={css.categories}></div>
-//     </div>
-//   );
-// };
+const MonthsSpend = () => {
+  return (
+    <div className={css.Container}>
+      <div>
+        <h1>Test</h1>
+      </div>
+    </div>
+  );
+};
 
-// export default MonthsSpend;
+export default MonthsSpend;
