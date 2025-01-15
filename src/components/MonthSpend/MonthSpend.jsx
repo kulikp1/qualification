231d604c0ce3da1b0398b15fa9b16c805a8570ca
kulikp1 <pashakylik2@gmail.com>
2025-01-15 20:17:33 +0200

    update structure add months spend

---
+++ src/components/MonthSpend/MonthSpend.jsx
@@ -0,0 +1,20 @@
+// import css from "./MonthSpend.css";
+
+// const MonthsSpend = () => {
+//   return (
+//     <div className={css.mainContainer}>
+//       <div>
+//         <h1 className={css.logo}>BudgetTrack</h1>
+//       </div>
+//       <div className={css.mainSpendsContainer}>
+//         <h1 className={css.title}>Hello, Pavlo!</h1>
+//         <h2 className={css.descr}>
+//           Today you spent 100 on the following categories:
+//         </h2>
+//       </div>
+//       <div className={css.categories}></div>
+//     </div>
+//   );
+// };
+
+// export default MonthsSpend;
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,11 +1,12 @@
 import DailySpend from "../../components/DailySpendPage/DailySpendPage";
+// import MonthsSpend from "../../components/MonthSpend/MonthSpend";
 import styles from "./TrackerPage.module.css";
 
 export default function TrackerPage() {
   return (
     <div className={styles.TrackerPage}>
       <DailySpend />
-      {/* <MonthSpend /> */}
+      {/* <MonthsSpend /> */}
     </div>
   );
 }
