0c4a1f79b5b1bb9a8fbd9b959cf5d1d5109bba7c
kulikp1 <pashakylik2@gmail.com>
2025-01-20 18:57:16 +0200

    correct orthography in import

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,12 +1,12 @@
 import SignInForm from "../../components/SignInPage/SignInForm/SignInForm";
-import AdvantagesSetion from "../../components/HomePage/AdvantagesSection/AdvantagesSection";
+import AdvantagesSection from "../../components/HomePage/AdvantagesSection/AdvantagesSection";
 import styles from "./SignInPage.module.css";
 
 export default function SignInPage() {
   return (
     <div className={styles.signInPageContainer}>
       <SignInForm />
-      <AdvantagesSetion />
+      <AdvantagesSection />
     </div>
   );
 }
