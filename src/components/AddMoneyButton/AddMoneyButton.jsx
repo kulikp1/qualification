c3b9680e2b2bec7c4a80afee09a42eaf7c2a50f3
kulikp1 <pashakylik2@gmail.com>
2025-02-26 16:00:12 +0200

    add markup for addMoneyBtn

---
+++ src/components/AddMoneyButton/AddMoneyButton.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import styles from "./AddWaterButton.module.css";
+import iconPlus from "../../assets/images/icons/icons.svg";
+import Modal from "../Modal/Modal";
+import AddSpendModal from "../Modals/AddSpendModal/AddSpendComponent/AddSpendComponent";
+import { useTranslation } from "react-i18next";
+
+const AddMoneyButton = () => {
+  const { t } = useTranslation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <button className={styles.addWaterButton}>
+        <div className={styles.containerSvg} onClick={openModal}>
+          <svg className={styles.icon}>
+            <use href={`${iconPlus}#icon-plus`}></use>
+          </svg>
+        </div>
+        <span className={styles.text}>{t("add_water")}</span>
+      </button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <AddSpendModal onClose={closeModal} />
+      </Modal>
+    </>
+  );
+};
+
+export default AddMoneyButton;
