e44becabee03124668e9c80714bd9ef4f8e4f6e0
kulikp1 <pashakylik2@gmail.com>
2025-02-26 16:15:58 +0200

    update spendItems component

--- src/components/MonthSpend/MonthSpend.jsx
+++ src/components/MonthSpend/MonthSpend.jsx
@@ -1,6 +1,6 @@
 import css from "./MonthSpend.module.css";
 import UserInfo from "../UserInfo/UserInfo";
-import { IoIosAdd } from "react-icons/io";
+// import { IoIosAdd } from "react-icons/io";
 import Calendar from "./Calendar/Calendar";
 import MonthName from "../MonthSpend/MonthName/MonthName";
 import AddSpendButton from "../AddMoneyButton/AddMoneyButton";
@@ -13,13 +13,13 @@ const MonthsSpend = () => {
       <div className={css.spendItems}>
         <h3 className={css.descr}>Month</h3>
         <MonthName></MonthName>
-        <div className={css.btnItem}>
+        {/* <div className={css.btnItem}>
           <button className={css.addBtn}>
-            <AddSpendButton></AddSpendButton>
-            <IoIosAdd />
+            
           </button>
           <h3 className={css.addBtnText}>Add spend</h3>
-        </div>
+        </div> */}
+        <AddSpendButton>{/* <IoIosAdd /> */}</AddSpendButton>
       </div>
 
       <Calendar></Calendar>
