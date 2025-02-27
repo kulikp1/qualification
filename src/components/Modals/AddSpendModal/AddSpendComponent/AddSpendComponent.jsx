313744f0e02735418ec3910f54a49e05b234bc2a
kulikp1 <pashakylik2@gmail.com>
2025-02-27 14:17:24 +0200

    add increase decrease state to counter

--- src/components/Modals/AddSpendModal/AddSpendComponent/AddSpendComponent.jsx
+++ src/components/Modals/AddSpendModal/AddSpendComponent/AddSpendComponent.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import AddSpendForm from "../AddSpendForm/AddSpendComponent";
 import css from "./AddSpend.module.css";
 import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
 
 const AddSpendComponent = () => {
+  const [amount, setAmount] = useState(50); // Початкове значення
+  const increaseAmount = () => {
+    setAmount((prev) => prev + 10); // Збільшення на 10 мл
+  };
+  const decreaseAmount = () => {
+    setAmount((prev) => (prev > 10 ? prev - 10 : prev)); // Зменшення на 10 мл, але не менше 10 мл
+  };
+
   return (
     <div className={css.spendContainer}>
       <div>
@@ -12,11 +21,11 @@ const AddSpendComponent = () => {
         <p className={css.spendDescr}>Choose a value:</p>
         <p className={css.amountDescr}>Amount:</p>
         <div className={css.btnContainer}>
-          <button className={css.formBtn}>
+          <button className={css.formBtn} onClick={decreaseAmount}>
             <CiCircleMinus className={css.btnMinus} />
           </button>
-          <p className={css.btnAmount}>50 ml</p>
-          <button className={css.formBtn}>
+          <p className={css.btnAmount}>{amount} $</p>
+          <button className={css.formBtn} onClick={increaseAmount}>
             <CiCirclePlus className={css.btnPlus} />
           </button>
         </div>
