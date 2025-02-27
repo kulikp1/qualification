d894ec0e21d9aad6d539cfdc36c86fe99e4c2a78
kulikp1 <pashakylik2@gmail.com>
2025-02-27 14:51:53 +0200

    styled category component

--- src/components/DailySpendPage/Category/Category.jsx
+++ src/components/DailySpendPage/Category/Category.jsx
@@ -1,16 +1,21 @@
 import css from "./Category.module.css";
 // import customer1 from "../../../assets/homePageAssets/pre-avatar.png";
 import { AiOutlineTransaction } from "react-icons/ai";
+import { FaPencil } from "react-icons/fa6";
 
 const Category = () => {
   return (
     <div className={css.container}>
+      <button>
+        <FaPencil className={css.changeBtn} />
+      </button>
+
       <h1 className={css.title}>Category</h1>
       <div className={css.categoryName}>
         <AiOutlineTransaction className={css.icon} />
 
         <div className={css.categorySpend}>
-          <h2 className={css.categoryDescr}>Shop</h2>
+          {/* <h2 className={css.categoryDescr}>Shop</h2> */}
           <p className={css.amount}>100$</p>
         </div>
       </div>
