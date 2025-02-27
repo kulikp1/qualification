4db86468ae4ee20b1816ed3fca28735344fac57a
kulikp1 <pashakylik2@gmail.com>
2025-02-28 01:03:41 +0200

    add state to counter spend in edit spend modal

--- src/components/Modals/EditSpendModal/EditSpendComponent/EditSpendComponent.jsx
+++ src/components/Modals/EditSpendModal/EditSpendComponent/EditSpendComponent.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 import AddSpendForm from "../EditSpendForm/EditSpendComponent";
 import css from "./EditSpendComponent.module.css";
 import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
 
 const EditSpendModal = () => {
+  const [amount, setAmount] = useState(250); // Початкове значення
+
+  const increaseAmount = () => {
+    setAmount((prev) => prev + 10);
+  };
+
+  const decreaseAmount = () => {
+    setAmount((prev) => (prev > 10 ? prev - 10 : prev));
+  };
+
   return (
     <div className={css.spendContainer}>
       <div>
@@ -12,11 +24,11 @@ const EditSpendModal = () => {
         <p className={css.spendDescr}>Correct entered data:</p>
         <p className={css.amountDescr}>Amount:</p>
         <div className={css.btnContainer}>
-          <button className={css.formBtn}>
+          <button className={css.formBtn} onClick={decreaseAmount}>
             <CiCircleMinus className={css.btnMinus} />
           </button>
-          <p className={css.btnAmount}>250 ml</p>
-          <button className={css.formBtn}>
+          <p className={css.btnAmount}>{amount} $</p>
+          <button className={css.formBtn} onClick={increaseAmount}>
             <CiCirclePlus className={css.btnPlus} />
           </button>
         </div>
