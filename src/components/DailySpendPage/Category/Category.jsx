c49c6f6c86dfa0442e8ba99694f3f24a86ef1108
kulikp1 <pashakylik2@gmail.com>
2025-02-27 14:36:27 +0200

    update category img = icon for all category

--- src/components/DailySpendPage/Category/Category.jsx
+++ src/components/DailySpendPage/Category/Category.jsx
@@ -1,16 +1,14 @@
 import css from "./Category.module.css";
-import customer1 from "../../../assets/homePageAssets/pre-avatar.png";
+// import customer1 from "../../../assets/homePageAssets/pre-avatar.png";
+import { AiOutlineTransaction } from "react-icons/ai";
 
 const Category = () => {
   return (
     <div className={css.container}>
       <h1 className={css.title}>Category</h1>
       <div className={css.categoryName}>
-        <img
-          src={customer1}
-          alt="categoryAvatar"
-          className={css.categoryAvatar}
-        />
+        <AiOutlineTransaction className={css.icon} />
+
         <div className={css.categorySpend}>
           <h2 className={css.categoryDescr}>Shop</h2>
           <p className={css.amount}>100$</p>
