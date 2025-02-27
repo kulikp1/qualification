9bff9d000f497db8d43b7e7b3de91bb71a4125a7
kulikp1 <pashakylik2@gmail.com>
2025-02-27 15:17:19 +0200

    add delete btn to category

--- src/components/DailySpendPage/Category/Category.jsx
+++ src/components/DailySpendPage/Category/Category.jsx
@@ -2,12 +2,45 @@ import css from "./Category.module.css";
 // import customer1 from "../../../assets/homePageAssets/pre-avatar.png";
 import { AiOutlineTransaction } from "react-icons/ai";
 import { FaPencil } from "react-icons/fa6";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
+import { useState } from "react";
+import DeleteModal from "../../Modals/DeleteModal/DeleteModal";
+import EditModal from "../../Modals/EditSpendModal/EditSpendComponent/EditSpendComponent";
+import Modal from "../../Modal/Modal";
 
 const Category = () => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  const openDeleteModal = () => {
+    if (!isDeleteModalOpen) {
+      setIsDeleteModalOpen(true);
+    }
+  };
+
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+
+  const openEditModal = () => {
+    if (!isEditModalOpen) {
+      setIsEditModalOpen(true);
+    }
+  };
+
+  const closeEditModal = () => {
+    setIsEditModalOpen(false);
+  };
+
   return (
     <div className={css.container}>
-      <button>
-        <FaPencil className={css.changeBtn} />
+      <button className={css.changeBtn} onClick={openEditModal}>
+        <FaPencil />
+      </button>
+
+      <button className={css.deleteBtn} onClick={openDeleteModal}>
+        <RiDeleteBin6Line />
       </button>
 
       <h1 className={css.title}>Category</h1>
@@ -19,6 +52,16 @@ const Category = () => {
           <p className={css.amount}>100$</p>
         </div>
       </div>
+      {isEditModalOpen && (
+        <Modal isOpen={isEditModalOpen} onClose={closeEditModal}>
+          <EditModal />
+        </Modal>
+      )}
+      {isDeleteModalOpen && (
+        <Modal isOpen={isDeleteModalOpen} onClose={closeDeleteModal}>
+          <DeleteModal onClose={closeDeleteModal} />
+        </Modal>
+      )}
     </div>
   );
 };
