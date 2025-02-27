8fdac09a5aab1acdae8606b9891536ec5a242846
kulikp1 <pashakylik2@gmail.com>
2025-02-27 15:21:20 +0200

    clean app.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,27 +5,19 @@ import TrackerPage from "../../pages/TrackerPage/TrackerPage.jsx";
 import SignInPage from "../../pages/SignInPage/SignInPage.jsx";
 import SignUpPage from "../../pages/SignUpPage/SignUpPage.jsx";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage.jsx";
-import DeleteModal from "../Modals/DeleteModal/DeleteModal.jsx";
-import LogOutModal from "../Modals/LogOutModal/LogOutModal.jsx";
-import EditSpendModal from "../Modals/EditSpendModal/EditSpendComponent/EditSpendComponent.jsx";
 import SharedLayout from "../SharedLayout/SharedLayout.jsx";
 
 function App() {
   return (
-    // <div className={css.appContainer}>
     <SharedLayout>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signUp" element={<SignUpPage />} />
         <Route path="/signIn" element={<SignInPage />} />
-        <Route path="/editSpend" element={<EditSpendModal />} />
-        <Route path="/delete" element={<DeleteModal />} />
-        <Route path="/logOut" element={<LogOutModal />} />
         <Route path="/tracker" element={<TrackerPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </SharedLayout>
-    // </div>
   );
 }
 
