9177770e72538c0cdb7c9a9fa0edd13ef6d424b5
kulikp1 <pashakylik2@gmail.com>
2025-03-11 21:44:42 +0200

    Update Category.jsx

--- src/components/DailySpendPage/Category/Category.jsx
+++ src/components/DailySpendPage/Category/Category.jsx
@@ -1,5 +1,4 @@
 import css from "./Category.module.css";
-// import customer1 from "../../../assets/homePageAssets/pre-avatar.png";
 import { AiOutlineTransaction } from "react-icons/ai";
 import { FaPencil } from "react-icons/fa6";
 import { RiDeleteBin6Line } from "react-icons/ri";
@@ -40,7 +39,6 @@ const Category = () => {
         <AiOutlineTransaction className={css.icon} />
 
         <div className={css.categorySpend}>
-          {/* <h2 className={css.categoryDescr}>Shop</h2> */}
           <p className={css.amount}>100$</p>
         </div>
       </div>
