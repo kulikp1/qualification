cf2458099145ac7ae6a9fef4d3e0f8100b022c91
kulikp1 <pashakylik2@gmail.com>
2025-03-11 21:43:29 +0200

    clean add money btn

--- src/components/AddMoneyButton/AddMoneyButton.jsx
+++ src/components/AddMoneyButton/AddMoneyButton.jsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import styles from "./AddMoneyButton.module.css";
-// import iconPlus from "../../assets/images/icons/icons.svg";
 import Modal from "../Modal/Modal";
 import AddSpendModal from "../Modals/AddSpendModal/AddSpendComponent/AddSpendComponent";
-// import { useTranslation } from "react-i18next";
 import { IoIosAdd } from "react-icons/io";
 
 const AddMoneyButton = () => {
-  // const { t } = useTranslation();
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -22,9 +19,6 @@ const AddMoneyButton = () => {
     <>
       <button className={styles.addSpendButton}>
         <div className={styles.containerSvg} onClick={openModal}>
-          {/* <svg className={styles.icon}>
-            <use href={`${iconPlus}#icon-plus`}></use>
-          </svg> */}
           <IoIosAdd className={styles.icon} />
         </div>
         <span className={styles.text}>{"Add spend"}</span>
