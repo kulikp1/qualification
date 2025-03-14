67b539724f78559d0a7986ac8910fdb91146969c
kulikp1 <pashakylik2@gmail.com>
2025-03-14 23:14:16 +0200

    Update SignInPage.jsx

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,4 +1,3 @@
-// import SignInForm from "../../components/SignIn/SignInForm/SignInForm";
 import AdvantagesSection from "../../components/HomePage/AdvantagesSection/AdvantagesSection";
 import styles from "./SignInPage.module.css";
 import SignInPageComponent from "../../components/SignIn/SignInPage/SignInPage";
