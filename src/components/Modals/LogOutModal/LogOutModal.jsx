36b5b3540542115e7709df1af5a71407866f9b8a
kulikp1 <pashakylik2@gmail.com>
2025-05-14 21:23:29 +0300

    add logout logic

--- src/components/Modals/LogOutModal/LogOutModal.jsx
+++ src/components/Modals/LogOutModal/LogOutModal.jsx
@@ -1,19 +1,22 @@
 import css from "./LogOutModal.module.css";
 import { IoClose } from "react-icons/io5";
 
-const LogOutModal = ({ onClose }) => {
+const LogOutModal = ({ onClose, onLogout }) => {
   return (
     <div className={css.logOutContainer}>
       <button className={css.close} onClick={onClose}>
         <IoClose className={css.closeBtnItem} />
       </button>
+
       <h1 className={css.logOutTitle}>Log out</h1>
       <p className={css.logOutDescr}>Do you really want to leave?</p>
 
       <div className={css.logOutBtns}>
-        <button className={css.btnLogOut}>Log out</button>
+        <button className={css.btnLogOut} onClick={onLogout}>
+          Log out
+        </button>
         <button className={css.btnCancel} onClick={onClose}>
-          Cancel{" "}
+          Cancel
         </button>
       </div>
     </div>
