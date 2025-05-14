f5a89511197279cfafb61cfd82c88535d3b3360b
kulikp1 <pashakylik2@gmail.com>
2025-05-14 21:13:32 +0300

    add PrivateRoute

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -6,6 +6,7 @@ import SignInPage from "../../pages/SignInPage/SignInPage.jsx";
 import SignUpPage from "../../pages/SignUpPage/SignUpPage.jsx";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage.jsx";
 import SharedLayout from "../SharedLayout/SharedLayout.jsx";
+import PrivateRoute from "../PrivateRoute/PrivateRoute.jsx"; // імпортуй компонент
 
 function App() {
   return (
@@ -14,7 +15,14 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signUp" element={<SignUpPage />} />
         <Route path="/signIn" element={<SignInPage />} />
-        <Route path="/tracker" element={<TrackerPage />} />
+        <Route
+          path="/tracker"
+          element={
+            <PrivateRoute>
+              <TrackerPage />
+            </PrivateRoute>
+          }
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </SharedLayout>
