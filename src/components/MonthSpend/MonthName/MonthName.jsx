f843dcadd25c08f2ed85b0db7798fe6e7d09323d
kulikp1 <pashakylik2@gmail.com>
2025-05-16 23:09:42 +0300

    add change month logic

--- src/components/MonthSpend/MonthName/MonthName.jsx
+++ src/components/MonthSpend/MonthName/MonthName.jsx
@@ -1,6 +1,60 @@
+import { useState } from "react";
 import css from "./MonthName.module.css";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 const MonthName = () => {
-  return <div className={css.nameContainer}>April</div>;
+  const today = new Date();
+  const [currentMonthIndex, setCurrentMonthIndex] = useState(today.getMonth());
+  const [currentYear, setCurrentYear] = useState(today.getFullYear());
+
+  const handlePrev = () => {
+    setCurrentMonthIndex((prev) => {
+      if (prev === 0) {
+        setCurrentYear((year) => year - 1);
+        return 11;
+      }
+      return prev - 1;
+    });
+  };
+
+  const handleNext = () => {
+    setCurrentMonthIndex((prev) => {
+      if (prev === 11) {
+        setCurrentYear((year) => year + 1);
+        return 0;
+      }
+      return prev + 1;
+    });
+  };
+
+  return (
+    <div className={css.nameContainer}>
+      <button onClick={handlePrev} className={css.arrowBtn}>
+        <IoIosArrowBack />
+      </button>
+      <span>
+        {monthNames[currentMonthIndex]} {currentYear}
+      </span>
+      <button onClick={handleNext} className={css.arrowBtn}>
+        <IoIosArrowForward />
+      </button>
+    </div>
+  );
 };
 
 export default MonthName;
