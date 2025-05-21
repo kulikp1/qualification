d5e0a8cc7eb6886e014a623a6610a7df8c54af30
kulikp1 <pashakylik2@gmail.com>
2025-05-21 16:03:05 +0300

    update settings modal ux ui

--- src/components/UserSettingsPage/UserSettingsPage.jsx
+++ src/components/UserSettingsPage/UserSettingsPage.jsx
@@ -6,26 +6,28 @@ import SettingsForm from "./SettingsForm/SettingsForm";
 
 const UserSettingsPage = ({ onClose }) => {
   return (
-    <div className={css.settingsContainer}>
-      <div className={css.settingsTitle}>
-        <h2 className={css.settingsTitleItem}>Setting</h2>
-        <button className={css.closeBtn} onClick={onClose}>
-          <IoClose className={css.closeBtnItem} />
-        </button>
-      </div>
-
-      <div className={css.avatarItems}>
-        <img src={userAvatar} alt="userAvatar" className={css.avatar} />
-        <div className={css.avatarItem}>
-          <button className={css.uploadBtn}>
-            <BsUpload />
+    <div className={css.overlay}>
+      <div className={css.modal}>
+        <div className={css.settingsTitle}>
+          <h2 className={css.settingsTitleItem}>Setting</h2>
+          <button className={css.closeBtn} onClick={onClose}>
+            <IoClose className={css.closeBtnItem} />
           </button>
-          <p className={css.avatarDescr}>Upload a photo</p>
         </div>
-      </div>
 
-      <div className={css.settingsController}>
-        <SettingsForm></SettingsForm>
+        <div className={css.avatarItems}>
+          <img src={userAvatar} alt="userAvatar" className={css.avatar} />
+          <div className={css.avatarItem}>
+            <button className={css.uploadBtn}>
+              <BsUpload />
+            </button>
+            <p className={css.avatarDescr}>Upload a photo</p>
+          </div>
+        </div>
+
+        <div className={css.settingsController}>
+          <SettingsForm />
+        </div>
       </div>
     </div>
   );
