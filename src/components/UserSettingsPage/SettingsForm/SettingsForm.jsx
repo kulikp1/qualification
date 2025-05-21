87798f00cb334318d7d6c767cda8ec62e90b7145
kulikp1 <pashakylik2@gmail.com>
2025-05-21 16:10:20 +0300

    update fields in settings form

--- src/components/UserSettingsPage/SettingsForm/SettingsForm.jsx
+++ src/components/UserSettingsPage/SettingsForm/SettingsForm.jsx
@@ -1,47 +1,20 @@
 import css from "./SettingsForm.module.css";
-import * as Yup from "yup";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const SettingsForm = () => {
-  const validationSchema = Yup.object({
-    name: Yup.string().required("Name is required"),
-    email: Yup.string()
-      .email("Invalid email format")
-      .required("Email is required"),
-    gender: Yup.string().required("Please select a gender"),
-  });
-
   return (
     <Formik
       initialValues={{
         name: "",
         email: "",
-        gender: "",
+        maxDailySpending: "",
       }}
-      validationSchema={validationSchema}
       onSubmit={(values) => {
         console.log("Form Values:", values);
       }}
     >
       {() => (
         <Form className={css.settingsForm}>
-          <div className={css.settingsController}>
-            <h3 className={css.settingsControllerDescr}>
-              Your gender identity
-            </h3>
-            <div role="group" className={css.radioGroup}>
-              <label>
-                <Field type="radio" name="gender" value="male" />
-                Woman
-              </label>
-              <label>
-                <Field type="radio" name="gender" value="female" />
-                Man
-              </label>
-            </div>
-            <ErrorMessage name="gender" component="div" className={css.error} />
-          </div>
-
           <div className={css.inputGroupName}>
             <label htmlFor="name">Your Name</label>
             <Field
@@ -62,6 +35,21 @@ const SettingsForm = () => {
               className={css.inputField}
             />
             <ErrorMessage name="email" component="div" className={css.error} />
+          </div>
+
+          <div className={css.inputGroupSpending}>
+            <label htmlFor="maxDailySpending">Maximum Daily Spending</label>
+            <Field
+              id="maxDailySpending"
+              name="maxDailySpending"
+              type="text"
+              className={css.inputField}
+            />
+            <ErrorMessage
+              name="maxDailySpending"
+              component="div"
+              className={css.error}
+            />
           </div>
 
           <button type="submit" className={css.submitBtn}>
