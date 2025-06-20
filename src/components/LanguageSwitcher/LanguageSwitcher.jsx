d5d5b8a0ecaa653c93c68d23d7114fb0506d5f99
kulikp1 <pashakylik2@gmail.com>
2025-06-20 23:39:54 +0300

    Update LanguageSwitcher.jsx

--- src/components/LanguageSwitcher/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.jsx
@@ -9,7 +9,7 @@ const LanguageSwitcher = () => {
   const toggleLanguage = () => {
     const newLang = i18n.language === "uk" ? "en" : "uk";
     i18n.changeLanguage(newLang);
-    localStorage.setItem("lang", newLang); // запам'ятати вибір
+    localStorage.setItem("lang", newLang); 
   };
 
   return (
